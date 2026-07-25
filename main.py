0a0a0da899bbb2f1f84c4a7752698c233f0fc967
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-07-25 12:01:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 
+# from fastapi import FastAPI
+# from schemas import *
+# from agent import Agent
+# import uvicorn
+
+# app = FastAPI()
+
+# agent = Agent()
+
+
+# @app.get("/")
+# def root():
+#     return {"message": "Hello AI Agent is Running"}
+
+
+# @app.post("/agent")
+# def run(req: AgentRequest):
+#     result = agent.run(req.request)
+#     return result
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from schemas import *
 from agent import Agent
 import uvicorn
 
 app = FastAPI()
-
 agent = Agent()
 
 
@@ -18,6 +42,16 @@ def root():
 def run(req: AgentRequest):
     result = agent.run(req.request)
     return result
+
+
+@app.get("/download")
+def download():
+    return FileResponse(
+        "output/res.docx",
+        filename="res.docx",
+        media_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    )
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
