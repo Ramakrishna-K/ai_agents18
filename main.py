9382094ec53f6d6db3364f5738871f1925884bb9
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-07-25 12:06:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,4 @@
 
-# from fastapi import FastAPI
-# from schemas import *
-# from agent import Agent
-# import uvicorn
-
-# app = FastAPI()
-
-# agent = Agent()
-
-
-# @app.get("/")
-# def root():
-#     return {"message": "Hello AI Agent is Running"}
-
-
-# @app.post("/agent")
-# def run(req: AgentRequest):
-#     result = agent.run(req.request)
-#     return result
-
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="127.0.0.1", port=8000)
-
-
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from schemas import *
