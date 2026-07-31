29fe3c65c20bf94fbb3ccd2a0dd9319afe93fa01
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-07-31 10:14:49 +0530

    Update document.py

--- document.py
+++ document.py
@@ -1,16 +1,3 @@
-# from docx import Document
-
-# class DocumentGenerator:
-#     def save(self, content):
-#         doc = Document()
-#         doc.add_heading("AI Generated Report",1)
-#         doc.add_paragraph(content)
-#         # path=""
-#         path = r"C:\Users\katra\OneDrive\Desktop\ai_agent\output\rerts.docx"
-#         doc.save(path)
-#         return path
-
-
 
 from docx import Document
 import os
